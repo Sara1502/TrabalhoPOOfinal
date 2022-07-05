6fc03d92fe432672000d6daf85da4c85b9d4d8bf
Sara1502 <104461441+Sara1502@users.noreply.github.com>
2022-07-05 19:38:07 -0300

    Update Trabalho.java

--- Trabalho.java
+++ Trabalho.java
@@ -5,7 +5,7 @@ public class Trabalho {
     double nota;
     double peso;
 
-    public Trabalhos(String nome, double nota, double peso) {
+    public Trabalho(String nome, double nota, double peso) {
         this.nome = nome;
         this.nota = nota;
         this.peso = peso;
