b6f6c4030a4c0bf98d70ae063de256afd91488aa
Sara1502 <104461441+Sara1502@users.noreply.github.com>
2022-07-05 19:35:34 -0300

    Create Trabalho.java

---
+++ Trabalho.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Trabalhos {
+    String nome;
+    double nota;
+    double peso;
+
+    public Trabalhos(String nome, double nota, double peso) {
+        this.nome = nome;
+        this.nota = nota;
+        this.peso = peso;
+    }
+}
