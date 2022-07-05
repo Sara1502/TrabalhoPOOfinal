b0a742a9ff1884906625396672768033c1fa1218
Sara1502 <104461441+Sara1502@users.noreply.github.com>
2022-07-05 19:38:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        Alunos aluno1 = new Alunos("Vitor");
+        Aluno aluno1 = new Aluno("Vitor");
         aluno1.notastrab("trab1", 7, 8);
         aluno1.notaspro("prova1", 7, 4);
         System.out.println(aluno1.nome+" |Media final: "+aluno1.mediafinal());
