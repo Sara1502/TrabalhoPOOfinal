b98834463a8d006f97591a01bc0567b8985647d7
Sara1502 <104461441+Sara1502@users.noreply.github.com>
2022-07-05 19:34:30 -0300

    Create prova.java

---
+++ prova.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Provas {
+    String nome;
+    double nota;
+    double peso;
+
+    public Provas(String nome, double nota, double peso) {
+        this.nome = nome;
+        this.nota = nota;
+        this.peso = peso;
+    }
+}
