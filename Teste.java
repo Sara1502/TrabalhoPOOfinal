3159527b0e8719a539beea49702173d26f41bde1
Sara1502 <104461441+Sara1502@users.noreply.github.com>
2022-07-05 21:35:55 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -7,7 +7,7 @@ class Teste {
     @org.junit.jupiter.api.Test
     void testmediafinal() {
         Aluno aluno1 = new Aluno("Lissa");
-        aluno1.notastrab("trab1", 7, 8);
+        aluno1.notastrab("trab1", 7, 4);
         aluno1.notaspro("prova1", 8, 6);
         assertEquals(7.2,aluno1.mediafinal());
     }
